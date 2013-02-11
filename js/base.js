25076babf71261cb2cd4711581b0b63ac8f064e4
freddy <freddy@mozpad.(none)>
2013-02-11 13:54:26 +0100

    gh-pages, yo yo yo

---
+++ js/base.js
@@ -0,0 +1,64 @@
+// Install app
+if (navigator.mozApps) {
+    var checkIfInstalled = navigator.mozApps.getSelf();
+    checkIfInstalled.onsuccess = function () {
+        if (checkIfInstalled.result) {
+            // Already installed
+        }
+        else {
+            var install = document.querySelector("#install"),
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+            /*
+                To install a package instead, exchange the above line to this:
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/package.webapp";
+            */
+            install.className = "show-install";
+            install.onclick = function () {
+                var installApp = navigator.mozApps.install(manifestURL);
+                /*
+                    To install a package instead, exchange the above line to this:
+                    var installApp = navigator.mozApps.installPackage(manifestURL);
+
+                    Make sure the manifestURL variable above has been changed too
+                */
+                installApp.onsuccess = function(data) {
+                    install.style.display = "none";
+                };
+                installApp.onerror = function() {
+                    alert("Install failed\n\n:" + installApp.error.name);
+                };
+            };
+        }
+    };
+}
+else {
+    console.log("Open Web Apps not supported");
+}
+
+// Reload content
+var reload = document.querySelector("#reload");
+if (reload) {
+    reload.onclick = function () {
+        location.reload(true);
+    };
+}
+
+///////////////////////////////////////////////////////////////////////
+
+// rock the screen size
+
+function choice(arr) {
+	r = Math.random();
+	return arr[Math.floor(r * arr.length)];
+}
+function getOutcome() {
+	outcome = ["It is certain","It is decidedly so","Without a doubt","Yes – definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"];
+	return choice(outcome);
+}
+function changeOutcome() {
+	text = getOutcome();
+	document.querySelector("#outcomeSpan").textContent = text;
+}
+changeOutcome();
+
+
