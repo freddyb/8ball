557ca6d2e44556fe967f13d564247c76ff34a498
freddy <freddy@mozpad.(none)>
2013-02-15 14:02:15 +0100

    lock screen orientation in portrait mode

--- js/base.js
+++ js/base.js
@@ -34,7 +34,9 @@ if (navigator.mozApps) {
 else {
     console.log("Open Web Apps not supported");
 }
-
+if ("lockOrientation" in window.screen) {
+	window.screen.lockOrientation("portrait"); // would be cooler if we could adapt though...
+}
 // Reload content
 var reload = document.querySelector("#reload");
 if (reload) {
@@ -49,7 +51,7 @@ function choice(arr) {
 	return arr[Math.floor(r * arr.length)];
 }
 function getOutcome() {
-	outcome = ["It is certain","It is decidedly so","Without a doubt","Yes – definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"];
+	outcome = ["It is certain","It is decidedly so","Without a doubt","Yes - definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"];
 	return choice(outcome);
 }
 function changeOutcome() {
